d77d3ffa5b1114dbff280ad6334c576f9924b44c
ajito nelson <8997ajito@gmail.com>
2025-09-17 17:07:06 +0900

    Update Link Website Chat

--- ajuda-digital-website/next.config.js
+++ ajuda-digital-website/next.config.js
@@ -16,12 +16,12 @@ const nextConfig = {
     return [
       {
         source: "/app",
-        destination: "https://chattest.ajuda-digital.com",
+        destination: "https://chat.ajuda-digital.com",
         permanent: true,
       },
       {
         source: "/chat",
-        destination: "https://chattest.ajuda-digital.com",
+        destination: "https://chat.ajuda-digital.com",
         permanent: true,
       },
     ];
